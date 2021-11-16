4f5e54e66a4fa5ad03a19b5b6c23492a33cafb84
MateoVG-coding <mateovega10@hotmail.co>
2021-11-16 15:30:36 -0500

    Added Unit Test for the function "analyzeTriangle"

---
+++ UnitTestTriangleSolverFunction/UnitTestTriangleSolverFunction.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" char* analyzeTriangle(int, int, int);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestTriangleSolverFunction
+{
+	TEST_CLASS(UnitTestTriangleSolverFunction)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
