512288385b85845f43efdf70f740c7a4f564f498
RyanCoding19 <Ryan_Wallace2019@outlook.com>
2021-11-18 11:29:36 -0500

    better finding angles function

--- PolygonChecker/getInsideAngles.c
+++ PolygonChecker/getInsideAngles.c
@@ -7,7 +7,8 @@
 int* getInsideAngles(int side1, int side2, int side3) {
 	int largestSide;
 	double second = 0, third = 0, largest0 = 0, largest1 = 0, largest2 = 0, finallargest = 0, cosinehelper = 0, cosineone = 0, cosinetwo = 0, Angle1 = 0;
-	double sin1 = 0, sin2 = 0, Angle2 = 0, Angle3 = 0;
+	double sin1 = 0, sin2 = 0, Angle2 = 0, Angle3 = 0, isodouble = 0, smallestSize = 0;
+
 	if (side1 >= side2 && side1 >= side3) {
 		largestSide = side1;
 		second = (double)side2;
@@ -43,9 +44,29 @@ int* getInsideAngles(int side1, int side2, int side3) {
 	sin2 = ((second * sin1) / ((double)largestSide));
 	Angle2 = (asin(sin2) * (180 / PI));//uses sin law with the angle I got from cosine and instead uses sine to find the 2nd angle
 
-	Angle3 = (180 - Angle1 - Angle2);//finally to get the third angle since all angles in a triangle must add up to 180 degrees I subtract the angles I have recieved by both of them to get the final angle of the triangle
-
+	Angle3 = (180 - Angle1 - Angle2);
+	//finally to get the third angle since all angles in a triangle must add up to 180 degrees I subtract the angles I have recieved by both of them to get the final angle of the triangle
+	if (side1 == side2 && side2 == side3 && side1 == side3)
+	{
+		Angle1 = 60; //for equalateral triangle
+		Angle2 = 60;
+		Angle3 = 60;
+	}
+	if (second == third)
+	{
+		isodouble = (180 - Angle1);//for isoslose triangle
+		isodouble = (isodouble / 2);
+		Angle2 = isodouble;
+		Angle3 = isodouble;
+	}
+	if (largestSide == second || largestSide == third)
+	{
+		Angle2 = Angle1;
+		Angle3 = (180 - (Angle1 + Angle2));
+	}
 	printf("Your 3 angles are: %f, %f and %f\n", Angle1, Angle2, Angle3);//prints all of the angles
-
-	return 0;
+	if ((Angle1 + Angle2 + Angle3) == 180)
+	{
+		return 1;
+	}
 }
